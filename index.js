c473489b63250945fa5b923bbbde9549b086facb
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-03-27 23:16:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ const terminatorButton = document.getElementById('Terminator 2')
 
 titanicButton.addEventListener('click', ()=> console.log(movies.Titanic.title)) 
 
-
-
-
 terminatorButton.addEventListener('click', ()=> console.log('hasta la vista'))
+
+function replaceTitanic(){
+  
+}
+
+
 
 //2. Get info for that movie
 //3. Replace the info on the page with the info for that movie.
