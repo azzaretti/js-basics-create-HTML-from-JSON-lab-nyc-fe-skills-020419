b5b6b44ddf64978e2a4d3674156d4f0984cca884
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-03-27 23:05:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
+  
+//1. Find out which button they click
+//2. Get info for that movie
+//3. Replace the info on the page with the info for that movie.
+  
+  
 });
