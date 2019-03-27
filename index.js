f7c913d8762672094e89ce5bbb054cf4b1fcfd17
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-03-27 23:20:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ terminatorButton.addEventListener('click', ()=> console.log('hasta la vista'))
 function onTitanic(){
   let titanicInfo = movies.Titanic
   let title = titanicInfo.title
-  
+  document.
   
 }
 
